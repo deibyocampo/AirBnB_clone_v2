290272131685afd6c9b8157e6281aebfe0335f5a
deibyocampo <deiby13_96@hotmail.com>
2020-01-22 23:09:21 -0500

    python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+starts a flask web aplication
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return ('Hello HBNB!')
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cool(text):
+    return ("C {}".format(text).replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def magic(text='is cool'):
+    return ("Python {}".format(text).replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run()
