33162df2367577e688ecad4b122e51ac6acd35b7
deibyocampo <deiby13_96@hotmail.com>
2020-03-03 22:29:23 -0500

    clean web static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,7 +2,7 @@
 '''
 script that deletes out-of-date archives, using the function do_clean:
 '''
-from fabric.api import run, env, hosts
+from fabric.api import *
 
 
 env.hosts = ['35.243.213.140', '34.73.54.82']
@@ -11,14 +11,14 @@ env.user = 'ubuntu'
 
 def do_clean(number=0):
     ''' delete old archive files '''
-    if number == 0:
-        number = number + 2
-    number = number + 1
-    result = local('ls -t /versions | tail -n +number| xargs rm')
-    if result.failed:
-        return False
-    result = run('ls -t /data/web_static/releases | tail -n +number| xargs rm')
-    if result.failed:
-        return False
 
-    return True
+    number = int(number)
+
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
